833ea90ce6125ebfdd797ab860292d90389d4575
jinosh05 <jinoshprabhuraj5@gmail.com>
2023-07-05 17:14:25 +0530

    LoginInterceptor created

--- lib/api_client.dart
+++ lib/api_client.dart
@@ -1,6 +1,36 @@
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:http_interceptor/http_interceptor.dart';
+
+class LoginInterceptor extends InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    final Map<String, String> headers = Map.from(data.headers);
+    headers[HttpHeaders.contentTypeHeader] = 'application/json';
+    final Map<String, dynamic> param = data.params;
+
+    param.addAll({
+      ///
+      ///  Other parameters have to be added here
+      ///
+    });
+    RequestData(
+      method: data.method,
+      baseUrl: data.baseUrl,
+      headers: headers,
+      body: data.body,
+      encoding: data.encoding,
+      params: param,
+    );
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    return data;
+  }
+}
 
 class ExpiredTokenRetryPolicy extends RetryPolicy {
   @override
